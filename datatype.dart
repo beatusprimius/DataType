1dcdbf2b2e31ef13387837ff99b2bcf6e7396801
beatusprimius <124495595+beatusprimius@users.noreply.github.com>
2023-03-21 17:00:52 +0300

    Add files via upload

---
+++ datatype.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Numbers (used to represent numeric values)
+int age = 30;
+double temperature = 98.6;
+print(age);
+print(temperature);
+
+// Strings (used to represent text values)
+String name = 'Beatus';
+print(name);
+
+// Booleans (used to represent true/false values)
+bool isTall = true;
+print(isTall);
+
+// List (used to represent collection of values)
+List<int> numbers = [1, 2, 3, 4, 5];
+print(numbers);
+
+// Maps (used to represent a collection of key-value pairs)
+Map<String, int> ages = {'John': 30, 'Jane': 25, 'Bob': 40};
+print(ages);
+
+//Runes (used to represent a unicode code point)
+ var heartSymbol = '\u{2665}';
+  print(heartSymbol);
+}
